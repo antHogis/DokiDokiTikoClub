106ef85a1cdcb215b168ed7cb81d8c29e4f0d249
antHogis <antonjhoglund@hotmail.com>
2018-10-01 20:32:19 +0300

    Add methods to Character.
    - Add void talkWith(Character)
    - Add void fight(Character)
    - Add int getHealthPoints()
    - Add float getAlcoholTolerance();
    - Add float getIntoxicationLevel()

--- src/main/java/fi/tamk/tiko/doki/Character.java
+++ src/main/java/fi/tamk/tiko/doki/Character.java
@@ -2,4 +2,15 @@ package fi.tamk.tiko.doki;
 
 public interface Character {
 
+    void talkWith(Character other);
+
+    void fight(Character other);
+
+    int getHealthPoints();
+
+    float getAlcoholTolerance();
+
+    float getIntoxicationLevel();
+
+
 }
