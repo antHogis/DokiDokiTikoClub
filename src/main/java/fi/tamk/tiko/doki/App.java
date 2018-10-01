92e27b435b6908a852876245e2c2c4dfab2e0ef8
antHogis <antonjhoglund@hotmail.com>
2018-10-01 20:11:52 +0300

    Create Maven project

---
+++ src/main/java/fi/tamk/tiko/doki/App.java
@@ -0,0 +1,13 @@
+package fi.tamk.tiko.doki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
