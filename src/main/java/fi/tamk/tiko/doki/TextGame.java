041db00a7258c7f9919c634dce9b4ef5b802826e
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-10-08 17:13:07 +0300

    Refactor Game to TextGame

---
+++ src/main/java/fi/tamk/tiko/doki/TextGame.java
@@ -0,0 +1,10 @@
+package fi.tamk.tiko.doki;
+
+public class TextGame {
+    Renderer renderer;
+    InputManager inputManager;
+
+    public void start() {
+        this.inputManager = new TextRenderer();
+    }
+}
