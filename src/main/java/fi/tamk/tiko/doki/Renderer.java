cdcdff86962303bb71a0bf7c5971d628b5e607e0
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-10-08 17:57:46 +0300

    Remove renderMainMenu from Renderer

--- src/main/java/fi/tamk/tiko/doki/Renderer.java
+++ src/main/java/fi/tamk/tiko/doki/Renderer.java
@@ -1,5 +1,4 @@
 package fi.tamk.tiko.doki;
 
 public interface Renderer {
-    abstract void renderMainMenu();
 }
--- src/main/java/fi/tamk/tiko/doki/TextRenderer.java
+++ src/main/java/fi/tamk/tiko/doki/TextRenderer.java
@@ -1,8 +1,5 @@
 package fi.tamk.tiko.doki;
 
 public class TextRenderer implements Renderer {
-    public void renderMainMenu() {
-        System.out.println("WELCOME TO VIDEO GAME.");
-        System.out.println("1. NEW GAME");
-    }
+
 }
