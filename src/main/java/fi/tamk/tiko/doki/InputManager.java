9beec8f452af5933fcbdea365f7092513485c780
antHogis <antonjhoglund@hotmail.com>
2018-10-08 16:13:06 +0300

    Add constructor to InputManager

--- src/main/java/fi/tamk/tiko/doki/InputManager.java
+++ src/main/java/fi/tamk/tiko/doki/InputManager.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class InputManager {
     private Scanner scanner;
     private String message;
-    
+
+    public InputManager() {
+        scanner = new Scanner(System.in);
+        message = "Give input";
+    }
+
 }
