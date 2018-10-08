4c976510f8aeb332d10630b7398e6bf8bced1063
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-10-08 17:15:53 +0300

    Add members to TextGame and instantiate them

--- src/main/java/fi/tamk/tiko/doki/TextGame.java
+++ src/main/java/fi/tamk/tiko/doki/TextGame.java
@@ -1,10 +1,25 @@
 package fi.tamk.tiko.doki;
 
 public class TextGame {
-    Renderer renderer;
-    InputManager inputManager;
+    private Renderer renderer;
+    private InputManager inputManager;
+    private Character pc;
+
+    public Renderer getRenderer() {
+        return renderer;
+    }
+
+    public InputManager getInputManager() {
+        return inputManager;
+    }
+
+    public Character getPc() {
+        return pc;
+    }
 
     public void start() {
-        this.inputManager = new TextRenderer();
+        this.inputManager = new InputManager();
+        this.renderer = new TextRenderer();
+        this.pc = new Character();
     }
 }
