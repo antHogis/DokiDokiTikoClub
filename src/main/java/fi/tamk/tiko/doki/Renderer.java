7c5127b2324ee21d84e145cff73dbee87d6b1023
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-10-08 18:12:34 +0300

    Event rendering

--- src/main/java/fi/tamk/tiko/doki/Renderer.java
+++ src/main/java/fi/tamk/tiko/doki/Renderer.java
@@ -1,4 +1,5 @@
 package fi.tamk.tiko.doki;
 
 public interface Renderer {
+    abstract void renderEvent(Event e);
 }
--- src/main/java/fi/tamk/tiko/doki/TextRenderer.java
+++ src/main/java/fi/tamk/tiko/doki/TextRenderer.java
@@ -1,5 +1,10 @@
 package fi.tamk.tiko.doki;
 
 public class TextRenderer implements Renderer {
-
+    public void renderEvent(Event e) {
+        System.out.println(e.getEventText());
+        for (Choice c : e.getChoices()) {
+            System.out.println(c.getChoiceText());
+        }
+    }
 }
